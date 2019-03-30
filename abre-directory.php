01239dc810934963249420db0a7118035efa2594
Chris Rose <chrisrose@Chriss-MacBook-Air.local>
2019-03-30 10:34:15 -0400

    Removed Avada Theme code.

--- abre-directory.php
+++ abre-directory.php
@@ -16,16 +16,31 @@ function directorylookup($atts, $content=null)
 	$building = str_replace(" ", "%20", $building);
 	$json=file_get_contents("https://subdomain.abre.io/modules/Abre-Directory/feed.php?building=$building");
 	$data = json_decode($json);
-	echo "<h2>$heading</h2>";
-	echo "<div class='fusion-table table-1'><table width='100%' class='tablesorter {sortlist: [[1,0]]}'><thead><tr><th>Last Name</th><th>First Name</th><th>Email</th><th>Title</th></tr></thead><tbody>";
+	?>
 
-	foreach ($data as $item)
-	{
-		echo "<tr><td>".$item->lastname."</td><td>".$item->firstname."</td><td>".$item->email."</td><td>".$item->title."</td></tr>";
-	}
+	<h2><?= $heading ?></h2>
+	<table width='100%'>
+		<thead>
+			<tr>
+				<th>Last Name</th>
+				<th>First Name</th>
+				<th>Email</th>
+				<th>Title</th>
+			</tr>
+		</thead>
+		<tbody>
 
-	echo "</tbody></table></div>";
+		<?php
+		foreach ($data as $item)
+		{
+			echo "<tr><td>".$item->lastname."</td><td>".$item->firstname."</td><td>".$item->email."</td><td>".$item->title."</td></tr>";
+		}
+		?>
 
+		</tbody>
+</table>
+
+<?php
 }
 
 //Create Shortcuts
