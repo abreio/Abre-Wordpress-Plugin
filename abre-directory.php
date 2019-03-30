92d54a748ae3d4b041cfd1d1151d1abe0dcbc234
Chris Rose <chrisrose@Chriss-MacBook-Air.local>
2019-03-30 10:30:08 -0400

    Add code needed to upload plugin to Wordpress

--- abre-directory.php
+++ abre-directory.php
@@ -1,5 +1,14 @@
 <?php
-	
+
+/**
+	* Plugin Name: Abre Directory
+	* Plugin URI: https://github.com/abreio/Abre-Wordpress-Plugin
+	* Description: Integrates Abre's Directory App with WordPress
+	* Version: 1.0
+	* Author: Abre.io
+	* Author URI: https://abre.io
+*/
+
 function directorylookup($atts, $content=null)
 {
 	extract(shortcode_atts( array('building' => '','heading' => ''), $atts));
@@ -8,14 +17,14 @@ function directorylookup($atts, $content=null)
 	$data = json_decode($json);
 	echo "<h2>$heading</h2>";
 	echo "<div class='fusion-table table-1'><table width='100%' class='tablesorter {sortlist: [[1,0]]}'><thead><tr><th>Last Name</th><th>First Name</th><th>Email</th><th>Title</th></tr></thead><tbody>";
-	
+
 	foreach ($data as $item)
 	{
 		echo "<tr><td>".$item->lastname."</td><td>".$item->firstname."</td><td>".$item->email."</td><td>".$item->title."</td></tr>";
 	}
-	
+
 	echo "</tbody></table></div>";
-	
+
 }
 add_shortcode( 'directory_lookup', 'directorylookup' );
 
